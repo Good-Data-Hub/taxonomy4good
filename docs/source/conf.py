2aba9323f77177ac4f4b0d8ceea44fbdfee186b3
Anis Bouhamadouche <anis.bouhamadouche@Aniss-MacBook-Air.local>
2022-06-22 04:24:55 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -27,7 +27,6 @@ author = 'Good Data Hub'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-master_doc = 'contents'
 extensions = [
     'recommonmark',
     'sphinx.ext.autodoc',
